1bc4c2a9b9c7ba90c54a41af50fa796a8260da1b
kevin-wang-2 <kevin_wang@sjtu.edu.cn>
2019-10-29 16:35:56 +0800

    Modified app.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,6 @@
  * A demonstration of this compiler tool.
  */
 
-let Compiler = require("./compiler.js").Compiler;
+let tool = require("homeworkCompiler.js").compile;
 
-let comp = new Compiler("C");
-
-comp.addSource('test.c');
-comp.compile('main');
-
-comp.events.on("compileFinish", (args) => {
-    console.log(args);
-});
+tool("C");
