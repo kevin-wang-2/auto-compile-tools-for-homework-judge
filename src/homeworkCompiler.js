655130a7159cff67660ac655a6ce8d787c114b90
kevin-wang-2 <kevin_wang@sjtu.edu.cn>
2019-10-29 17:24:57 +0800

    Update RegExp for filename check;Update event emitter; Add information database for autograder

--- src/homeworkCompiler.js
+++ src/homeworkCompiler.js
@@ -12,6 +12,7 @@ let extname = {
     "C++14": ".cpp",
     "C++17": ".cpp"
 };
+let parser = /.*_[A-Z][a-z]+[A-Z]*_s\d+_hw[4-8].*.[c|C].*/;
 
 let compileEvent = new events.EventEmitter();
 function compile(lang) {
@@ -19,25 +20,59 @@ function compile(lang) {
         if(err) {
             console.log(err);
         } else {
+            let dataJson = []; // All data for homework files
             let compilationJobCnt = 0;
+            let fioJobCnt = 0;
+
             files.forEach(function(filename) {
-                if(path.extname(filename).toLowerCase() !== extname[lang]) return;
+                let curId = dataJson.length;
+                dataJson.push({source: filename, bin: "", errorMsg: ""});
+
+                if(path.extname(filename).toLowerCase() !== extname[lang]) {
+                    dataJson[curId].errorMsg = "Wrong Extension Name! ";
+                    return;
+                }
+                if(!parser.exec(filename)) dataJson[curId].errorMsg = "Bad File Name!";
+
                 let fullFilename = "../source/" + filename;
                 let comp = new Compiler(lang);
                 comp.addSource(fullFilename);
                 compilationJobCnt++;
                 comp.compile("../bin/" + path.basename(filename, path.extname(filename)));
                 comp.events.on("compileFinish", function(arg) {
-                    compilationJobCnt--;
-                    if(compilationJobCnt === 0) compileEvent.emit("batchFinish");
                     if(arg.status !== 0) {
                         if(arg.stderr) {
-                            fs.writeFile("../bin/" + filename + ".compile.log", arg.stderr, () => {});
+                            fioJobCnt++;
+                            fs.writeFile("../bin/" + filename + ".compile.log", arg.stderr, () => {
+                                fioJobCnt--;
+                                compileEvent.emit("checkStatus", [compilationJobCnt, fioJobCnt, dataJson]);
+                            });
+                            dataJson[curId].errorMsg += arg.stderr;
                         } else {
-                            fs.writeFile("../bin/" + filename + ".compile.log", arg.stack, () => {});
+                            fioJobCnt++;
+                            fs.writeFile("../bin/" + filename + ".compile.log", arg.stack, () => {
+                                fioJobCnt--;
+                                compileEvent.emit("checkStatus", [compilationJobCnt, fioJobCnt, dataJson]);});
+                            dataJson[curId].errorMsg += arg.stack;
                         }
+                    } else {
+                        dataJson[curId].bin = "../bin/" + path.basename(filename, path.extname(filename));
                     }
+
+                    compilationJobCnt--;
+                    compileEvent.emit("checkStatus", [compilationJobCnt, fioJobCnt, dataJson]);
                 });
+            });
+
+            compileEvent.on("checkStatus", (args) => {
+                let compilationJobCnt = args[0];
+                let fioJobCnt = args[1];
+                let dataJson = args[2];
+                if((compilationJobCnt === 0) && (fioJobCnt === 0)) {
+                    fs.writeFile("../data/compile.json", JSON.stringify(dataJson), () => {
+                        compileEvent.emit("batchFinish");
+                    });
+                }
             });
         }
     });
